a5c05021305f25c11b211ad410a45ede23302c46
85264709611 <93631581+85264709611@users.noreply.github.com>
2023-02-21 16:21:21 +0800

    Create mtxx.js

---
+++ vip/mtxx.js
@@ -0,0 +1,3 @@
+hostname = *.xiuxiu.meitu.com
+
+^https?:\/\/(h5|api)\.xiuxiu\.meitu\.com\/v\d\/(h\d\/vip|vip|user)\/ url script-response-body https://raw.githubusercontent.com/gjwj666/qx/main/mtxx.js
